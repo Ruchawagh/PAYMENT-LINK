f58acb114b3e1a3d1e4062ffba9d08791ada4d78
rucha11 <99780429+Ruchawagh@users.noreply.github.com>
2022-09-17 20:26:57 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,43 @@
+let payment = document.getElementById("payment");
+let paymentidea = document.getElementById('paymentidea');
+let home = document.getElementById('home');
+payment.addEventListener('click',()=>{
+    paymentidea.style.top = '100px';
+    payment.style.opacity = '0';
+    setTimeout(() => {
+        paymentidea.style.display = 'none';
+        home.style.display = 'block';
+    }, 1000);
+})
+
+
+ 
+document.querySelector('.card-number-input').oninput = () =>{
+    document.querySelector('.card-number-box').innerText = document.querySelector('.card-number-input').value;
+}
+
+document.querySelector('.card-holder-input').oninput = () =>{
+    document.querySelector('.card-holder-name').innerText = document.querySelector('.card-holder-input').value;
+}
+
+document.querySelector('.month-input').oninput = () =>{
+    document.querySelector('.exp-month').innerText = document.querySelector('.month-input').value;
+}
+
+document.querySelector('.year-input').oninput = () =>{
+    document.querySelector('.exp-year').innerText = document.querySelector('.year-input').value;
+}
+
+document.querySelector('.cvv-input').onmouseenter = () =>{
+    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(-180deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(0deg)';
+}
+
+document.querySelector('.cvv-input').onmouseleave = () =>{
+    document.querySelector('.front').style.transform = 'perspective(1000px) rotateY(0deg)';
+    document.querySelector('.back').style.transform = 'perspective(1000px) rotateY(180deg)';
+}
+
+document.querySelector('.cvv-input').oninput = () =>{
+    document.querySelector('.cvv-box').innerText = document.querySelector('.cvv-input').value;
+}
